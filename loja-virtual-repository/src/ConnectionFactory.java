84dc2fd716cfc8041a90d12733331155c3e3da62
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-07-11 10:52:09 -0300

    Java e JDBC: trabalhando com um banco de dados Java e JDBC: trabalhando com um banco de dados
    Criando o pool de conexões

--- loja-virtual-repository/src/ConnectionFactory.java
+++ loja-virtual-repository/src/ConnectionFactory.java
@@ -1,13 +1,27 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		
+		ComboPooledDataSource cpds = new ComboPooledDataSource();
+		cpds.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		cpds.setUser("jefferson");
+		cpds.setPassword("Ninaluna3802@");
+		
+		this.dataSource = cpds;
+		
+	}
+	
 	public Connection recuperarConexao() throws SQLException {
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", 
-				"jefferson", "Ninaluna3802@");
+		return this.dataSource.getConnection();
 		
 		
 	}
